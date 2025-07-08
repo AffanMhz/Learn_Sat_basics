f8f6e40215c8d51d19f19e4a82c32d43ce51577f
Affan <affan2304788@st.jmi.ac.in>
2025-07-08 20:21:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Learn_Sat_basics/',
   // Uncomment and update the base path when deploying to GitHub Pages
   // base: '/your-repo-name/',
-})
+});
+
+
